93447d56eb21149a7b5106db3bd6fd73dd053f8b
takuya <>
2022-08-15 19:19:23 +0900

    update

--- my-flexstart-app/src/App.tsx
+++ my-flexstart-app/src/App.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 //import './App.css';
 
 function App() {
+
+  React.useEffect(() => {
+    console.log("myfunc");
+//    myfunc();
+  }, []);
+
   return (
 
 <div>
@@ -33,7 +39,7 @@ function App() {
       <div className="row">
         <div className="col-lg-6 d-flex flex-column justify-content-center">
           <h1 data-aos="fade-up">株式会社 Sakaiine</h1>
-          <h2 data-aos="fade-up" data-aos-delay={400}>我々はIT技術で社会に貢献します。</h2>
+          <h2 data-aos="fade-up" data-aos-delay={400}>IT技術で社会に貢献します。</h2>
           <div data-aos="fade-up" data-aos-delay={600}>
             <div className="text-center text-lg-start">
               <a href="#about" className="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
@@ -114,7 +120,7 @@ function App() {
           <div className="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay={300}>
             <div className="service-box orange">
               <i className="ri-discuss-line icon" />
-              <h3>クラウド移行支援</h3>
+              <h3>クラウド活用支援</h3>
               <p>AWS, Azure
               </p>
               {/* <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a> */}
