6aeb2165a3887c42bbfa65c1a8060cedb21e0ca3
takuya <>
2022-08-16 20:28:09 +0900

    update

--- my-flexstart-app/src/App.tsx
+++ my-flexstart-app/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
       <div className="row">
         <div className="col-lg-6 d-flex flex-column justify-content-center">
           <h1 data-aos="fade-up">株式会社 Sakaiine</h1>
-          <h2 data-aos="fade-up" data-aos-delay={400}>IT技術で社会に貢献します。</h2>
+          <h2 data-aos="fade-up" data-aos-delay={400}>IT技術で地域に貢献します。</h2>
           <div data-aos="fade-up" data-aos-delay={600}>
             <div className="text-center text-lg-start">
               <a href="#about" className="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
